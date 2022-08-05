fbfb2ff0bb0c514f24eae035947610dece62d91f
Fabiola Diniz <fabioladiniz36@hotmail.com>
2022-08-05 15:32:37 +0000

    Atualizações

--- backend/database/bd.js
+++ backend/database/bd.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://localhost:27018/test');
+  await mongoose.connect('mongodb://localhost:27018/notas');
 }
 
 module.exports = mongoose;
